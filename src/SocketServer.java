45addf0af3d413ec0382c94e07f16730218bf2bf
shandaZhang <1365873702@qq.com>
2015-11-09 09:58:12 +0800

    修改了一点点

--- src/SocketServer.java
+++ src/SocketServer.java
@@ -5,23 +5,37 @@ import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class SocketServer {
-	public static void main(String []args) throws  IOException{
-		ServerSocket ss =  new ServerSocket(30000);
-		while(true){
-			Socket s =ss.accept();
-			BufferedReader br = new BufferedReader(
-					new InputStreamReader(ss.accept().getInputStream()));
-			String line = br.readLine();
-			System.out.println(line);
-			System.out.println();
-			PrintStream ps = new PrintStream(s.getOutputStream());
-			ps.println("服务器欢迎你");
-			ps.close();
-			s.close();
-			
+	public static void main(String[] args) throws IOException {
+		ServerSocket ss = new ServerSocket(30000);
+		while (true) {
+			Socket s = ss.accept();
+			PrintStream ps = null;
+			BufferedReader br = null;
+
+			try {
+				br = new BufferedReader(new InputStreamReader(
+						s.getInputStream()));
+				String line;
+				while (true) {
+					line = br.readLine();
+					//if (line.equals("END"))
+					//	break;
+					System.out.println(line);
+					break;
+				}
+				
+				ps = new PrintStream(s.getOutputStream());
+				ps.println("服务器欢迎你");
+				System.out.println("已发送");
+			} catch (Exception e) {
+				System.out.println("e: "+e);
+			} finally {
+				ps.close();
+				br.close();
+				s.close();
+			}
+
 		}
 	}
-
 }
